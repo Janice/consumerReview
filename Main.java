bb44d94de285ec21fb41d6e390737e9407e43270
Janice <janice@baileywx.com>
2020-10-14 18:41:39 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args)
+  {
+    System.out.print("Sentiment Value of 'happily':  ");
+    System.out.println(Review.sentimentVal("happily")); 
+  }
+}
